05285c276643e161d4933398bc7ce1b209eb103f
Tettey1 <tetteynathan89@gmail.com>
2023-05-27 09:01:26 +0000

    new file:   percentage_of_letter_instring.py

---
+++ percentage_of_letter_instring.py
@@ -0,0 +1,10 @@
+def percentageLetter(s: str, letter: str) -> int:
+    count = 0
+    num = len(s)
+    
+    for item in s:
+        if letter == item:
+            count+=1
+    return round((count/num)*100)
+    
+print(percentageLetter('foobar','o'))
