da6e80bc3f03503b1716823ab2678baa4d1b1b97
Tettey1 <tetteynathan89@gmail.com>
2023-06-02 16:20:06 +0000

    modified:   list.py 	modified:   nested_list.py

--- list.py
+++ list.py
@@ -1,14 +1,44 @@
-if __name__ == '__main__':
-    N = int(input())
-    
-    for i in range(N):
-        commands = input().split()
-    n_list = []
-    print(commands)
-    for i in range(len(commands)):
-        if 'insert' in commands:
-            
-    
 
-    print(n_list)
-    
+# *args and *kwargs
+# def addition(*args):
+#     total = 0
+#     for numbers in args:
+#         total += numbers
+#     print(f'Sum ={total}')
+
+# addition(1,2,3,4,20)
+# order x,y,*args,**kwargs
+# def myFunction(x,*args,**kwargs):
+#     print(x)
+#     print(args)
+#     for keys,values in kwargs.items():
+#         print(f'{keys} = {values}')
+
+# myFunction('Ghana','University of Gh', name='Nat',age='20')
+#OOP
+class BakingPan:
+    unit_price = 5
+    def __init__(self,flour,sugar,special_ingredient) -> None:
+        self.flour = flour # bread2.flour = 'soft'
+        self.sugar = sugar
+        self.special_ingredient = special_ingredient
+
+    def bread_name(self):
+        return f'{self.special_ingredient} bread'
+    def total_price(self,quantity):
+        total = quantity* self.unit_price
+        return f'Total Price of {quantity} {self.bread_name()} = Ghc {total}'
+
+# flour, sugar, special indredient assigning attribute and parameters
+
+bread1= BakingPan('soft',20,'Wheat')
+bread2= BakingPan('hard',10,'Coconut')
+
+print(bread1.total_price(10))
+print(bread2.total_price(30))
+
+
+
+
+
+
--- nested_list.py
+++ nested_list.py
@@ -1,12 +1,35 @@
 if __name__ == '__main__':
-    records = []
+    students = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        records.append([name,score])
-    print(max(records))
-    for i in range(len(records)):
-        max_score=records[i][1]
-        for item in records[i]:
-            if item>max_score:
-                max_score=item
+
+        students.append([name,score])
+
+    def sort_list(list,index):
+        list.sort(key=lambda x:x[index])
+        return list
+    
+    students = sort_list(students,1)
+    min_score_name = min(students, key=lambda x:x[1])
+
+    runners_up = []
+
+    for i in range(len(students)):
+        if min_score_name[1]<students[i][1]:
+            runners_up.append(students[i])
+
+    min_runner_up = min(runners_up, key=lambda x:x[1])
+    min_sort = sort_list(runners_up,0)
+    for item in runners_up:
+        if item[1]==min_runner_up[1]:
+            print(item[0])
+
+    
+
+    
+    
+    
+
+    
+        
