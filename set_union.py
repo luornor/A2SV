5511dd9aa52d7257baf4b5945d079b688bf3bb3c
Tettey1 <tetteynathan89@gmail.com>
2023-06-03 22:18:50 +0000

    modified:   set_difference.py 	new file:   set_union.py

--- set_union.py
+++ set_union.py
@@ -1,10 +1,11 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n_english = int(input())
 
-s = set(input().split())
+e = set(input().split())
 
 n_french = int(input())
+f = set(input().split())
 
-f =set(input().split())
+# if 0<n_english+n_french<1000:
+print(len(e.union(f)))
 
-print(len(s.difference(f)))
