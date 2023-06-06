2758ab3f413cc6ec10952b352a4ab7b5a45d8f26
Tettey1 <tetteynathan89@gmail.com>
2023-06-06 02:51:06 +0000

    modified:   word_order.py

--- word_order.py
+++ word_order.py
@@ -3,9 +3,11 @@ n = int(input())
 word_list = [input() for _ in range(n)]
 
 dict = {}
-for item in (word_list):
-    a = word_list.count(item)
-    dict[item]= a
+for item in word_list:
+    if item in dict:
+        dict[item]+=1
+    else:
+        dict[item]=1
 print(len(dict))
 for v in dict.values():
     print(v,end=" ")
