2af0768aca19ebb59f728c59b406bb1b4c62895f
Tettey1 <tetteynathan89@gmail.com>
2023-06-06 00:49:35 +0000

    modified:   dakti_song.py 	modified:   ordered_dict.py 	new file:   pattern.py

--- dakti_song.py
+++ dakti_song.py
@@ -1,6 +1,8 @@
 """Juan loves the Dakti song and wants to memorize the chorus of the song. His friend sent him the chorus in phrases, but the phrases are somewhat strange; they do not have an order and they have numbers. His friend helps Juan organize the chorus of the song.
 
 First, organize the words based on the numbers they have, then delete the numbers once they are organized."""
+
+
 N = int(input())
 
 def sorting_condition(word):
--- ordered_dict.py
+++ ordered_dict.py
@@ -1,1 +1,22 @@
-from collections import OrderedDict
+from collections import OrderedDict,Counter
+
+item_order = OrderedDict()
+    
+    
+res = []
+item_num= int(input())
+for _ in range(item_num):
+    item_price  = input().split()
+    res.append(item_price)
+
+for *item,price in res:
+        item = " ".join(item)
+        if item in item_order:
+            item_order[item]+=int(price)
+        else:
+            item_order[item]=int(price)
+for k,v in item_order.items():
+     print(k,v)
+    
+
+
