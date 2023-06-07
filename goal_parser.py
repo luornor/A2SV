2a1f7107cb1a43e423e7ec7de20dcf7a197a1498
Tettey1 <tetteynathan89@gmail.com>
2023-06-07 01:40:28 +0000

    modified:   goal_parser.py

--- goal_parser.py
+++ goal_parser.py
@@ -5,9 +5,23 @@ def interpret(command: str) -> str:
         '()':'o',
         '(al)':'al',
     }
-    res = ''
+    interpreted = []
+    i = 0
+    while i< len(command):
+        if command[i]=='G':
+            interpreted.append(dict[command[i]])
+            i+=1
+        elif command[i:i+2]=='()':
+            interpreted.append(dict[command[i:i+2]])
+            i+=2
+        elif command[i:i+4]=='(al)':
+            interpreted.append(dict[command[i:i+4]])
+            i+=4
+
+    print(''.join(interpreted))
     
-    print(res)
+
+    
 
 interpret(input())
         
