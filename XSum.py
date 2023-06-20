538e6788be2fac314968ae9052a3de2597e6000f
Tettey1 <tetteynathan89@gmail.com>
2023-06-20 02:27:17 +0000

    modified:   XSum.py

--- XSum.py
+++ XSum.py
@@ -24,19 +24,49 @@ For each test case output a single integer, the maximum sum over all possible pl
 
 """
 def xSum(board):
-    max_sum = float('-inf')
-    row = 0
-    col = 0
-    while row < len(board)-1 or col < len(board[0])-1:
-        diagonal_sum = board[row][col] + board[row-1][col-1] + board[row+1][col+1]+ board[row+1][col-1]+ board[row-1][col+1]
-        if diagonal_sum > max_sum:
-            max_sum = diagonal_sum
-        row+=1
-        col+=1
+    max_sum = 0
+    row = len(board) # rows
+    col = len(board[0]) #cols
+    # For each cell at position (i, j), 
+    # calculate the sum of all cells attacked by a bishop placed at that position.
+    # sum_attacked = 0
+    for i in range(row):
+        for j in range(col):
+             # the value of the current cell
+            sum_attacked = board[i][j]
+            # check up-left diagonal
+            x,y = i,j
+            while x>0 and y>0:
+                x-=1
+                y-=1
+                sum_attacked+=board[x][y]
                 
+            #check up-right diagonal
+            x,y = i, j
+            while x>0 and y<col-1:
+                x-=1
+                y+=1
+                sum_attacked+=board[x][y]
+                
+            #check down-left diagonal
+            x,y = i, j
+            while x<row-1 and y>0:
+                x+=1
+                y-=1
+                sum_attacked+=board[x][y]
+                
+            #check down-right diagonal
+            x,y = i, j
+            while x<row-1 and y<col-1:
+                x+=1
+                y+=1
+                sum_attacked+= board[x][y]
+                
+                
+            max_sum = max(max_sum,sum_attacked)
     return max_sum
 
-    
+
 
 
 
