772baddd9a1a214f33e22f16a69c85345f275229
Tettey1 <tetteynathan89@gmail.com>
2023-06-26 12:57:30 +0000

    modify

--- matrixReshape.py
+++ matrixReshape.py
@@ -1,11 +1,10 @@
-def matrixReshape(mat: list[list[int]], r: int, c: int) -> list[list[int]]:
+def matrixReshape(mat, r, c):
     rows = len(mat)
     cols = len(mat[0])
     new_mat = []
 
-    if (rows*cols)==(r*c):
+    if (rows*cols) == (r*c):
         arr = [mat[row][col] for row in range(rows) for col in range(cols)]
-        
         idx = 0
         for i in range(r):
             new_arr = []
@@ -17,6 +16,5 @@ def matrixReshape(mat: list[list[int]], r: int, c: int) -> list[list[int]]:
     else:
         return mat
 
-   
 
 print(matrixReshape([[1,2],[3,4]],4,1))
