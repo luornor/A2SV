127797f946b3e8c9a367c4d423da982c2c1c80b7
Tettey1 <tetteynathan89@gmail.com>
2023-06-28 21:53:02 +0000

    modified:   contest_4/D_Present_from_Lena.py 	modified:   contest_4/E_Fall_Down.py

--- contest_4/D_Present_from_Lena.py
+++ contest_4/D_Present_from_Lena.py
@@ -1,58 +1,35 @@
-
 n = int(input())
 
-# first half of the present
 
 for i in range(n+1):
-    t = -1
-    for j in range((2*n)+1):
-        if (j>=n-i) and (j<=n+i):
-            if j<=n:
-                t+=1
-                print(t,end='')
-            else: 
-                t-=1
-                print(t,end='')
-            if j!= n+i:
-                print(" ",end='')
-        elif j<= n-i:
-            print(" ")
+    # Determine the number of spaces before the digits on each line
+    spaces = " "*(n-i)*2
+
+    # Generate the string of digits for the current line
+    nums = " ".join(str(j) for j in range(i))
     
-    
-
-#second half of the present
-    
-
-
-
-
-
+    # nums_reverse = " ".join(str(j) for j in range(i,-1,-1))
+    if i!=0:
+        # Print the pattern for non-zero rows
+        print(spaces + nums+ " "  + str(i) + " "+ nums[::-1])
+    else:
+        # Print the pattern for the first row (i.e., when i is 0)
+        print(spaces + str(i))
 
 
+for i in range(n-1,-1,-1):
+
+    # Determine the number of spaces before the digits on each line
+    spaces = " "*(n-i)*2    
+    # Generate the string of digits for the current line
+    nums = " ".join(str(j) for j in range(i))
+    # nums_reverse = " ".join(str(j) for j in range(i,-1,-1))
+    if i!=0:
+        # Print the pattern for non-zero rows
+        print(spaces + nums+ " "  + str(i) + " "+ nums[::-1])
+    else:
+        # Print the pattern for the first row (i.e., when i is 0)
+        print(spaces + str(i))
 
 
 
-# n = int(input())
-# y = 1
-# for i in range((2*n)+1):
-#     row = [" "] * ((2*n)+1)
-
-#     z = 0
-#     while z < y:
-#         row[n] = y-1
-#         row[n-z] = row[n+z] = y-z-1
-#         z+=1
-    
-#     if i < n:
-#         y+=1
-#     else:
-#         y-=1
-    
-#     i = 0
-#     while i < len(row):
-#         if i > n and row[i] == " ":
-#             row.pop(i)
-#         else:
-#             i+=1
-
-#     print(*row)
---
+++ contest_4/E_Fall_Down.py
@@ -0,0 +1,35 @@
+t = int(input())
+
+def simulate_fall(grid):
+    rows,cols = len(grid), len(grid[0])
+    # occupied = [[False]*cols for _ in range(rows)]
+    # move through each column from left to right
+    for c in range(cols):
+        #move through each row from buttom to top except last row
+        for r in range(rows-1,-1,-1):
+            #if we find a stone
+            if grid[r][c] == '*':
+                #we loop through the column from buttom to top row
+                # and let the stone fall
+                for i in range(rows-1,-1,-1):
+                    if grid[i][c] == '*' and grid[i+1][c]=='.':
+                        grid[i][c]= '.'
+                        grid[i+1][c]='*'
+                else:
+                    continue
+    return grid
+
+
+
+for _ in range(t):
+    rows,cols = map(int,input().split()) # Read the number of rows and columns
+    grid = [list(input()) for _ in range(rows)]   # Read the input grid
+    #simulate the fall process
+    grid = simulate_fall(grid)
+
+    #output the grid
+    for row in grid:
+        print(''.join(row))
+    if _!= t-1:
+        print()
+
