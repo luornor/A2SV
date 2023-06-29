b016aa2aec548c54768fe51da234c027b6aba43d
Tettey1 <tetteynathan89@gmail.com>
2023-06-29 23:04:24 +0000

    new file:   sort_thePeople.py

---
+++ sort_thePeople.py
@@ -0,0 +1,12 @@
+def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        #[180,165,170]
+        people = [(names[i],heights[i]) for i in range(len(heights))]
+        
+        for i in range(len(heights)):
+            for j in range(len(heights)-1):
+                if people[j][1]<people[j+1][1]:
+                    people[j],people[j+1] = people[j+1],people[j]
+                    
+        sorted_names = [item[0] for item in people]
+
+        return sorted_names
