cfa4e21c0c50876b7ae0cb3c307482c58cd86c1d
Tettey1 <tetteynathan89@gmail.com>
2023-07-03 14:46:14 +0000

    modified:   contest_5/A_Destroyer.py

--- contest_5/A_Destroyer.py
+++ contest_5/A_Destroyer.py
@@ -4,45 +4,37 @@ from collections import Counter, defaultdict
 t = int(input())
 
 def solve(l,n):
-    #if 2 robots say that there are 2 robots infront 
-    #at least one must say 1 robots are in front or 0 robots infront
-    #count the number of appearances of each number
-    #check if the count of number x is less than or equal number x-1
     # Problem A: 
     # 1: Count the number of occurances of each number in the array
     # 2: Check if the count of number x is less than or equals to the count of (x-1) for all unique x in the array
     
-    # count = [0]*(max(l)+1) 
-    # for i in range(n):
-    #     count[l[i]] +=1
-    # for i in range(1,len(count)):
-    #     if count[i-1]<count[i]:
-    #         return 'NO'
-    # return 'YES' 
-    a = [0] * n
-    for i in range(n):
-        a[i] = l[i]
-
-    a.sort()
-
-    for i in range(n):
-        a[i] = i
-
-    possible = True
-    for i in range(n):
-        if a[i] > i:
-            possible = False
-            break
     
-    if possible:
-        print("YES")
-    else:
-        print("NO")
+    l = sorted(l,reverse=True)
 
+    if n==1 and l[0] != 0:
+        return 'NO'
     
-    
+    for i in range(n-1):
+        x = l[i] #take the first element
+        if x==-1:
+            continue
+
+        for j in range(i+1,n):
+            if l[j]==x-1: #compare max-1 to the next element
+                l[j] = -1 #if they are equal mark the element
+                x-=1 #then reduce the max_element
+            if x==0: #break if max==0
+                break
+        if x!=0:
+            #if the inner loop finishes check if
+            #the sequence is correct
+            return 'NO'
+        
+    return 'YES'
+        
+        
 for _ in range(t):
     n = int(input())
     l = list(map(int,input().split()))
-    solve(l,n)
+    print(solve(l,n))
        
