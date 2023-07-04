123a0ff367c18c70568446ad8dddbb3d4fd54807
Tettey1 <tetteynathan89@gmail.com>
2023-07-04 15:53:24 +0000

    modified:   contest_5/D_Sort_with_Step.py

--- contest_5/D_Sort_with_Step.py
+++ contest_5/D_Sort_with_Step.py
@@ -1,17 +1,26 @@
+
 t = int(input())
 
-def solve(n,k,p):
-    i = 0
-    j = n-1
-    while i<j:
-        if abs(i-j)==k:
-            p[i],p[j]= p[j],p[i]
-            break
-        i+=1
-        j-=1
-    
 
+def solve(n,k,p):
+    count=0
+    for i in range(n):
+        if (p[i]-i-1)%k==0:
+            continue
+        count+=1
+    
+    
+    if count==0:
+        print(0)
+    elif count<=2:
+        print(1)
+    else:
+        print(-1)
+
+	
+
+       
 for _ in range(t):
     n,k = map(int,input().split())
     p = list(map(int,input().split()))
-    print(solve(n,k,p))
+    solve(n,k,p)
