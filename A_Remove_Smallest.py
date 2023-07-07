571efdbb33ea073b3088c7502b0fc70fc5b6d99f
Tettey1 <tetteynathan89@gmail.com>
2023-07-07 16:52:12 +0000

    modified:   A_Remove_Smallest.py

--- A_Remove_Smallest.py
+++ A_Remove_Smallest.py
@@ -1,32 +1,24 @@
 t = int(input())
 
 def solve(n,a):
-    i = 0
-    j = n-1
-    while i<j:
-        #if they are equal remove any
-        if a[i]==a[j]:
-            a.pop(i)
-            j-=1
-            #else remove the smallest element
-        elif abs(a[i]-a[j])<=1:
-            if a[i]<a[j]:
-                a.pop(i)
-                j-=1
-            else:          
-                break
-        i+=1
-        
+    a.sort()
+    for i in range(1,n):
+        if abs(a[i]-a[i-1])>1:
+           return 'NO'    
+    return 'YES'
+    
+    # for i in range(n-1):
+    #     if abs(a[i]-a[i+1])<=1:
+    #        continue
+    #     else:
+    #         return 'NO' 
+    # return 'YES'
 
-        
-        
-    if len(a)==1:
-        print('YES')
-    else:
-        print('NO')
-
+    
 
 for _ in range(t):
     n = int(input())
     a = list(map(int,input().split()))
-    solve(n,a)
+    print(solve(n,a))
+
+    
