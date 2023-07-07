d6da5259589cea492b5086c3113dc150de382296
Tettey1 <tetteynathan89@gmail.com>
2023-07-07 20:34:25 +0000

    new file:   C_Less_or_Equal.py 	new file:   largestNumber.py

---
+++ C_Less_or_Equal.py
@@ -0,0 +1,5 @@
+n,k = map(int,input().split())
+
+a = list(map(int,input().split()))
+count = 0
+for x in range(1,10^9)
---
+++ largestNumber.py
@@ -0,0 +1,27 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        [3,30,34,5,9]
+        # 3,5,9,30,34
+        #[9,909]
+        #9099
+        #9909
+        nums = sorted(nums,reverse=True)
+        n = len(nums)
+        if all(num == 0 for num in nums):
+            return '0'
+        for i in range(n):
+            for j in range(n-1):
+                num1 = str(nums[j])
+                num2 = str(nums[j+1])
+                if int(num1+num2)<int(num2+num1):
+                    nums[j],nums[j+1] = nums[j+1],nums[j]
+        res = ""
+        for ch in nums:
+            res+=str(ch)
+
+        return res
+
+
+        
+
+        
