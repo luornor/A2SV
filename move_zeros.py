ca417cc31f594797c04fea1761c4340c23156c69
Tettey1 <tetteynathan89@gmail.com>
2023-07-12 19:09:40 +0000

    modified:   move_zeros.py

--- move_zeros.py
+++ move_zeros.py
@@ -1,15 +1,10 @@
-"""
-Given an integer array nums, move all 0's to the end of it while maintaining the relative order
-of the non-zero elements.
 
-Note that you must do this in-place without making a copy of the array."""
+# nums = [0,1,0,3,12]
 
-nums = [0,1,0,3,12]
-
-for num in nums:
-    if num==0:
-        nums.append(nums.pop(nums.index(num)))
-print(nums)
+# for num in nums:
+#     if num==0:
+#         nums.append(nums.pop(nums.index(num)))
+# print(nums)
 
 plc = 0 #placeholder pointer
 seek = 0#seeker pointer
