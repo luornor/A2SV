20752a1a564c7205ea34c6854676149dfb1772b3
Tettey1 <tetteynathan89@gmail.com>
2023-07-19 11:38:28 +0000

    modified:   codeforces-solutions/C_Alternating_Subsequence.py

--- codeforces-solutions/C_Alternating_Subsequence.py
+++ codeforces-solutions/C_Alternating_Subsequence.py
@@ -7,15 +7,7 @@ def solve():
     l = 0
     # r = 1
     res = []
-    #if first is negative we look for negative-positve-negative
-    # while r<n:
-    #     if sequence[r]>sequence[l]:
-    #         res.append(sequence[r])
-    #         l=r
-
-    #     elif sequence[r]<sequence[l]:
-    #         res.append(sequence[r])
-    #     r+=1
+    
     
 
 
