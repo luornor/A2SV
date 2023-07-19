ab585b7250d7578b705a00d4055e485a42bd83d7
Tettey1 <tetteynathan89@gmail.com>
2023-07-19 21:33:49 +0000

    modified:   contest_5/F_Virus.py

--- contest_5/F_Virus.py
+++ contest_5/F_Virus.py
@@ -1,3 +1,24 @@
+"""
+This may help
+
+
+Step to do the virus problem 
+
+- storing the size of each segment in an array( will be array of integers), let say the name is arr
+
+- create a variable(infected) that taking care of the infected houses in each segment 
+
+- until arr becomes empty or the max segment in the array less than infected, do these things 
+
+1, choose the maximum number(max segment) and remove it from  arr
+2, In the removed segment you are going to protect max(1, removed - 1 - inf)
+3, add the number of protected houses to your answer
+4, increment infected by 4, since you are going to lose 4 houses in each segment in each iteration
+
+
+- finally you will have number of total protected houses that are accumulated in each iteration
+
+"""
 t = int(input())
 
 def solve():
@@ -8,16 +29,15 @@ def solve():
     infected_houses = list(map(int, input().split()))
     infected_houses.sort()
     #gaps between infected houses
-    gaps = [(infected_houses[i] - infected_houses[i - 1] - 1) % n for i in range(m)]
+    segment = [(infected_houses[i] - infected_houses[i - 1] - 1) % n for i in range(m)]
     
     protected = 0   
-    num_infected = 0
-    protected = 0
-    while len(gaps) > 0 and max(gaps)>num_infected:
-        removed = gaps.pop(0)
-        protected+=max(1, removed - 1 - num_infected)
-        num_infected+=4
-    print(n-protected)
+    infected = 0
+    while len(segment) < 0 or max(segment)<infected:
+        removed = segment.pop(max(segment))
+        protected+=max(1, removed - 1 - float('inf'))
+        infected += 4
+    print(protected)
 
 for _ in range(t):
     solve()
