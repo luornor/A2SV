ece8a1d06956763a626b3859fc9fd48fa4fa67be
Tettey1 <tetteynathan89@gmail.com>
2023-07-24 01:34:38 +0000

    modified:   codeforces-solutions/C_Alternating_Subsequence.py 	modified:   codeforces-solutions/traffic_light_problem.py

--- codeforces-solutions/C_Alternating_Subsequence.py
+++ codeforces-solutions/C_Alternating_Subsequence.py
@@ -1,16 +1,33 @@
+
+def sign(num):
+    return num>=0
+
+
+def solve(n,nums):
+    l,r = 0,0
+    arr = []
+    #find segments of the same sign
+    while r<n:
+        while r< n-1 and sign(nums[r])==sign(nums[r+1]):
+            r+=1
+        arr.append(nums[l:r+1])
+        r+=1
+        l = r
+    #find maximum element in each segment and sum them
+    max_sum = 0
+    for item in arr:
+        if item:
+            max_sum+=max(item)
+
+    print(max_sum)
+    
+    
+
+
+
 t = int(input())
-
-
-def solve():
     
-    l = 0
-    # r = 1
-    res = []
-    
-    
-
-
 for _ in range(t):
     n = int(input())
     sequence = list(map(int,input().split()))
-    solve()
+    solve(n,sequence)
--- codeforces-solutions/traffic_light_problem.py
+++ codeforces-solutions/traffic_light_problem.py
@@ -1,12 +1,13 @@
 t = int(input())
 
-def solve():
-    len_s,curr_color = input().split()
+def solve(len_s,curr_color,colors):
     len_s = int(len_s)
-    colors = input()
-    # concatenate the string
     colors += colors
-    ans = float('-inf')
+    ans = 0
+    if len_s==1 and colors[0]=='g':
+        print(0)
+        return
+    
     for i in range(len_s): #number of iterations
         if colors[i]==curr_color:
             for j in range(i+1,2*len_s):
@@ -14,5 +15,9 @@ def solve():
                     ans = max(ans,j-i)
                     break # why?? becos we need to find minimum time
 
+    print(ans)
+
 for _ in range(t):
-    solve()
+    len_s,curr_color = input().split()
+    colors = input()
+    solve(len_s,curr_color,colors)
