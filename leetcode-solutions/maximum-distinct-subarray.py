bcfdb17bdeb0926790a971fa59c5fc5b06710566
Tettey1 <tetteynathan89@gmail.com>
2023-08-02 00:04:47 +0000

    new file:   leetcode-solutions/maximum-distinct-subarray.py

---
+++ leetcode-solutions/maximum-distinct-subarray.py
@@ -0,0 +1,15 @@
+def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        currsum = 0
+        maxsum = 0
+        l = 0
+        window = set()
+        for r in range(len(nums)):
+            while l < r and (nums[r] in window or len(window) >= k):
+                currsum -= nums[l]
+                window.remove(nums[l])
+                l += 1
+            currsum += nums[r]
+            window.add(nums[r])
+            if len(window) == k:
+                maxsum = max(maxsum, currsum)
+        return maxsum
