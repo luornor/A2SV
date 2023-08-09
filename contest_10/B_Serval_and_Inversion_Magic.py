1a6c086fe06764e16e89799691a5378ef311fe36
Tettey1 <tetteynathan89@gmail.com>
2023-08-09 12:02:36 +0000

    modified:   contest_10/B_Serval_and_Inversion_Magic.py

--- contest_10/B_Serval_and_Inversion_Magic.py
+++ contest_10/B_Serval_and_Inversion_Magic.py
@@ -1,19 +1,44 @@
 def solve(n,s):
-    # Split the string into two halves
-    s1, s2 = s[:n//2], s[n//2 if n % 2 == 0 else n//2 + 1:][::-1]
-    # Count the number of 0s and 1s in each half
-    c1 = s1.count('0')
-    c2 = s2.count('0')
-    # Check if the number of 0s in s1 is equal to the number of 1s in s2 and vice versa
-    if c1 == len(s2) - c2 and c2 == len(s1) - c1:
-        print('Yes')
-    else:
-        print('No')
+    if s == s[::-1]:
+        return "Yes"
+
+    illegal_positions = []
+    for i in range(1, n // 2 + 1):
+        if s[i - 1] != s[n - i]:
+            illegal_positions.append(i)
+
+    if len(illegal_positions) <= 1:
+        return "Yes"
+
+    if illegal_positions[-1] - illegal_positions[0] == len(illegal_positions) - 1:
+        return "Yes"
+
+    return "No"
     
+
+    #Tutorial
+    # l = 0
+    # r = n
+    # while l < r and s[l] == s[r - 1]:
+    #     l += 1
+    #     r -= 1
+    # if l >= r:
+    #     print("Yes")
+    #     return
+    # while l < r and s[l] != s[r - 1]:
+    #     l += 1
+    #     r -= 1
+    # while l < r and s[l] == s[r - 1]:
+    #     l += 1
+    #     r -= 1
+    # if l >= r:
+    #     print("Yes")
+    # else:
+    #     print("No")
 
 
 t = int(input())
 for _ in range(t):
     n = int(input())
     s = input()
-    solve(n,s)
+    print(solve(n,s))
