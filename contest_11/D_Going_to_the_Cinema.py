0880994c760a1afc475c2d8deca3f371cf927135
Tettey1 <tetteynathan89@gmail.com>
2023-08-18 12:27:51 +0000

    modified:   contest_11/.cph/.D_Going_to_the_Cinema.py_ddb78e105a065fb996412cd576ed445e.prob 	modified:   contest_11/D_Going_to_the_Cinema.py

--- contest_11/D_Going_to_the_Cinema.py
+++ contest_11/D_Going_to_the_Cinema.py
@@ -1,21 +1,16 @@
-def solve(n,claims):
-     # Count the number of people who want to go for each ai value
-    ai_counts = [0] * n
-    for ai in claims:
-        ai_counts[ai] += 1
+def solve(n,a):
+    a.sort()
+    ans = 0
+    if a[0]!=0:
+        ans=2
+    else:
+        ans=1
+    for i in range(n-1):
+        if a[i]<i+1 and a[i+1]>= i+2:
+            ans+=1
+
+    print(ans)
     
-    cumulative_count = 0
-    not_sad = [False] * n
-    
-    # Iterate through the claims in decreasing order of ai
-    for i in range(n - 1, -1, -1):
-        cumulative_count += ai_counts[i]
-        if cumulative_count >= i:
-            for j in range(i, n):
-                not_sad[j] = True
-            break
-    
-    print(not_sad.count(True))
 
 
 
