7446b3536d82d79c9bac4598c1e93eefdd6fa1a7
Tettey1 <tetteynathan89@gmail.com>
2023-08-24 16:00:45 +0000

    modified:   contest_12/E_Remove_Two_Letters.py

--- contest_12/E_Remove_Two_Letters.py
+++ contest_12/E_Remove_Two_Letters.py
@@ -1,9 +1,14 @@
-def solve(n):
-    pass
+def solve(s,n):
+    repeated = 0
+    for i in range(2,n):
+        if s[i]==s[i-2]:
+            repeated+=1
+    print(n-1-repeated)
 
 
 t = int(input())
 for _ in range(t):
     n = int(input())
     s = input()
-    solve(s)
+    solve(s,n)
+
