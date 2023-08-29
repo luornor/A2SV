875006be663cc26f5e037f52e9d38dba1b53db30
Tettey1 <tetteynathan89@gmail.com>
2023-08-29 17:50:58 +0000

    modified:   contest_13/F_Good_Subarrays.py 	modified:   leetcode-solutions/matrix_diagonal_sum.py

--- contest_13/F_Good_Subarrays.py
+++ contest_13/F_Good_Subarrays.py
@@ -2,26 +2,67 @@ def solve(a,n):
     #1 2 0
     #prefix_sum = [0,1,3,3]
     #[1,2,4,4]
-    # count = 0
-    # prefix_sum = [0] * (n+1)
-    # prefix_sum[0] = a[0]
-    # for i in range(n):
-    #     prefix_sum[i + 1] = prefix_sum[i] + a[i]
-    # prefix_sum = [0]
-    # val = 0
-    # for num in a:
-    #     val+=num
-    #     prefix_sum.append(val)
-    # occurrences = {}
-    # print(prefix_sum)
-    # for i in range(n+1):
-    #     diff = prefix_sum[i] - i
-    #     count += occurrences.get(diff, 0)
-    #     occurrences[diff] = occurrences.get(diff, 0) + 1
-    # print(count)
+    count = 0
+    prefix_sum = [0] * (n+1)
+    prefix_sum[0] = a[0]
+    for i in range(n):
+        prefix_sum[i+1] = prefix_sum[i] + a[i]
+  
+    occurrences = {}
+    print(prefix_sum)
+    for i in range(n+1):
+        diff = prefix_sum[i] - i
+        count += occurrences.get(diff, 0)
+        occurrences[diff] = occurrences.get(diff, 0) + 1
+    print(count)
 
     #1 2 0
-    
+    """
+    #include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define mod 1000000007
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1e9+7
+//#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    	int n;
+    	cin>>n;
+    	string s;
+    	cin>>s;
+    	vector<int> a(n);
+    	rep(i, n) a[i] = s[i] - '0' - 1;
+    	map<int, ll> mm;
+    	ll ans = 0;
+    	mm[0] = 1;
+    	ll r = 0;
+    	rep(i, n)
+    	{
+        	r += a[i];
+        	ans += mm[r];
+        	mm[r]++;
+    	}
+    	cout << ans <<endl;
+	}
+}
+    """
+
 
 
 
--- leetcode-solutions/matrix_diagonal_sum.py
+++ leetcode-solutions/matrix_diagonal_sum.py
@@ -8,7 +8,29 @@ for i in range(len(mat)):
     sum+=mat[i][i]
     if i!= len(mat)-1-i:
         sum+=mat[i][len(mat)-1-i]
-           
+# print(sum)
+ 
+cols = len(mat[0])
+rows = len(mat[0])
+p_s = [[0]*(cols+1) for _ in range(rows+1)]
+
+# p_s[0][0] = mat[0][0]
+
+# for i in range(1,rows):
+#     p_s[i][0]=mat[i][0]+p_s[i-1][0]
+# for j in range(1,cols):
+#     p_s[0][j] += mat[0][j]+p_s[0][j-1]
+for i in range(rows):
+    for j in range(cols):
+        p_s[i][j]=p_s[i-1][j]+p_s[i][j-1]-p_s[i-1][j-1]+mat[i][j]
+    
+        
+
+
+
+print(p_s)
+        
+
             
                 
             
@@ -16,16 +38,4 @@ for i in range(len(mat)):
 
 
 
-
-    
-    
-        
-    
-        
-
-
-
-
-
-        
-print(sum)
+ 
