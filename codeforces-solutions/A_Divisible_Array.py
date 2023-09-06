cfa66b77abbba8af401ca8d9b5f2f7e1e8434dfa
Tettey1 <tetteynathan89@gmail.com>
2023-09-06 16:39:05 +0000

    modified:   codeforces-solutions/A_Divisible_Array.py

--- codeforces-solutions/A_Divisible_Array.py
+++ codeforces-solutions/A_Divisible_Array.py
@@ -1,16 +1,11 @@
 def solve(n):
-    N = 2000001
-    s = 0
-    a = [0]*(N+1)
-    for i in range(n, 1, -1):
-        a[i] = i
-        s = (s + i) % n
-    a[1] = n - s
+    a =[]
     
     
     for i in range(1, n + 1):
-        print(a[i], end=' ')
-    print()
+        a.append(2*i)
+    
+    print(*a)
 
 
 
