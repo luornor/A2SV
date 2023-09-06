2c9f1dd5f0e4705b31b28d64e68dd8ff48ddf81e
Tettey1 <87794745+Tettey1@users.noreply.github.com>
2023-09-06 12:16:13 +0000

    Added solution for Insertion Sort List

---
+++ leetcode-solutions/insertion-sort-list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        arr = []
+
+        while head:
+            arr.append(head.val)
+            head = head.next
+
+        
+        for i in range(1,len(arr)):
+            j = i
+            while j>0 and arr[j-1]>arr[j]:
+                arr[j-1],arr[j] = arr[j],arr[j-1]
+                j-=1
+        start = None
+        for num in arr:
+            new_node = ListNode(val=num)
+            if not start:
+                start = new_node
+            else:
+                tmp = start
+                while tmp and tmp.next:
+                    tmp = tmp.next
+                tmp.next = new_node
+
+        return start
+
+            
