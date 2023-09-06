9a0ec9c14963dc8d9fd47a80595bf3f1bfb29700
Tettey1 <87794745+Tettey1@users.noreply.github.com>
2023-09-06 16:36:00 +0000

    Added solution for Insertion Sort List

--- leetcode-solutions/insertion-sort-list.py
+++ leetcode-solutions/insertion-sort-list.py
@@ -5,29 +5,55 @@ class ListNode:
         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
+        # arr = []
+        # while head:
+        #     arr.append(head.val)
+        #     head = head.next
+        # for i in range(1,len(arr)):
+        #     j = i
+        #     while j>0 and arr[j-1]>arr[j]:
+        #         arr[j-1],arr[j] = arr[j],arr[j-1]
+        #         j-=1
+        # start = None
+        # for num in arr:
+        #     new_node = ListNode(val=num)
+        #     if not start:
+        #         start = new_node
+        #     else:
+        #         tmp = start
+        #         while tmp and tmp.next:
+        #             tmp = tmp.next
+        #         tmp.next = new_node
 
-        while head:
-            arr.append(head.val)
-            head = head.next
+        # return start
 
-        
-        for i in range(1,len(arr)):
-            j = i
-            while j>0 and arr[j-1]>arr[j]:
-                arr[j-1],arr[j] = arr[j],arr[j-1]
-                j-=1
-        start = None
-        for num in arr:
-            new_node = ListNode(val=num)
-            if not start:
-                start = new_node
+
+        # temp = current.next
+        # current.next = prev.next
+        # prev.next = current
+        # current = temp
+        dummy = ListNode(next=head)
+        curr = head.next
+        prev = head        
+        while curr:
+            if curr.val>=prev.val:
+                prev = curr
+                curr = curr.next
             else:
-                tmp = start
-                while tmp and tmp.next:
+                tmp = dummy
+                while tmp.next.val<curr.val:
                     tmp = tmp.next
-                tmp.next = new_node
+                #insert before previous node
+                prev.next = curr.next
+                curr.next = tmp.next
+                tmp.next = curr
+                curr = prev.next
+                
+        
+        return dummy.next
 
-        return start
+
+
+
 
             
