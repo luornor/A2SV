86920f1177e2be5bb61556fb49e57cf8e597424d
Tettey1 <tetteynathan89@gmail.com>
2023-09-07 14:13:53 +0000

    modified:   contest_14/E_Greg_and_Array.py

--- contest_14/E_Greg_and_Array.py
+++ contest_14/E_Greg_and_Array.py
@@ -33,33 +33,16 @@ for _ in range(m):
 result = [0] * n
 
 # Calculate prefix sum for operations
-ps = [0] * (m + 1)
+df = [0] * (m + 1)
 
 for i in range(k):
     x, y = map(int, input().split())
     x -= 1  
-    # y -= 1
-    ps[x] += 1  # Increment the start of the range
-    ps[y] -= 1  # Decrement one past the end of the range
-
+    df[x] += 1  # Increment the start of the range
+    df[y] -= 1  # Decrement one past the end of the range
+print(df)
 #actual values for each operation using prefix sum
 for i in range(1, m):
-    ps[i] += ps[i - 1]
-# print(ps)
-
-# Apply queries and calculate the final result array
-for i in range(m):
-    l, r, d = operations[i]
-    result[l - 1] += ps[i] * d
-    if r< n:
-        result[r] -= ps[i] * d
-
-# Calculate the cumulative sum for the final result
-for i in range(1, n):
-    result[i] += result[i - 1]
-# print(result)
-#Add initial_array to the result to get the final values
-for i in range(n):
-    result[i] += a[i]
+    df[i] += df[i - 1]
 
 print(*result)
