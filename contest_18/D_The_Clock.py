b4d43db9e4c9d158b6787a7ecc82d91d0aab907e
Tettey1 <tetteynathan89@gmail.com>
2023-10-23 22:09:13 +0000

    modified:   contest_18/D_The_Clock.py 	modified:   contest_18/E_Yet_Another_Problem_About_Pairs_Satisfying_an_Inequality.py

--- contest_18/D_The_Clock.py
+++ contest_18/D_The_Clock.py
@@ -1,10 +1,48 @@
+def format(hour,minute):
+    if hour<10:
+        hour = '0'+ str(hour)
+    if minute<10:
+        minute= '0'+str(minute)
+    return f"{hour}:{minute}"
+
 def solve(clock,x):
-    x = int(x)
+    # time,k = clock.split()
+    hour,mins = clock.split(':')
+    
+    #change mins into hours and modularize mins
+    check_hours = int(x)//60
+    check_mins = int(x)%60
+    hour = int(hour)
+    mins = int(mins)
+    count = 0
+    while True:
+        #count minutes before hours
+        mins+= check_mins
+        if mins>=60:
+            mins = mins%60
+            hour+=1
+
+        hour+=check_hours
+        if hour>=24:
+            hour = hour%24
+        
+        
+        curr_time = format(hour,mins)
+        if curr_time[::-1]==curr_time:
+            count+=1
+
+        if curr_time==clock:
+            break
+
+    print(count)  
+
+
+        
 
 
 
 t = int(input())
 
 for _ in range(t):
-    clock,x=map(input().split())
+    clock,x=input().split()
     solve(clock,x)
--- contest_18/E_Yet_Another_Problem_About_Pairs_Satisfying_an_Inequality.py
+++ contest_18/E_Yet_Another_Problem_About_Pairs_Satisfying_an_Inequality.py
@@ -1,13 +1,36 @@
+from math import factorial
 def solve(a,n):
-    count=0
-    for i in range(n):
-        for j in range(i+1,n):
-            if a[i]<i+1<a[j]<j+1:
-                count+=1
+    # for i in range(n):
+    #     for j in range(i+1,n):
+    #         if a[i]<i+1<a[j]<j+1:
+    #             count+=1
+    pairs = []
+    check = set()
+    for i,num in enumerate(a):
+        if i+1>num and num not in check:
+            pairs.append((num,i+1))
+            check.add(num)
     
+    if len(pairs)>=2:
+        count=int(factorial(len(pairs))/(factorial(2)*factorial(len(pairs)-2)))
+    else:
+        count=0
+
+    pairs.sort()
+    #check adjanc
+    for i in range(len(pairs)-1):
+        num,idx = pairs[i]
+        if pairs[i+1][0]-pairs[i][0]==1:
+            count-=1
+        if idx in check and pairs[i+1][0]-pairs[i][0]!=1:
+            count-=1
+
+    
+    print(count)
     
 
-    print(count)
+
+    
 
 t = int(input())
 for _ in range(t):
