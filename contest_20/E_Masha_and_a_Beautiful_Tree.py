0f4cc99edd2a7ad370673fd8b6618b8a17dd064a
Tettey1 <tetteynathan89@gmail.com>
2023-11-16 15:17:29 +0000

    modified:   contest_20/E_Masha_and_a_Beautiful_Tree.py 	modified:   mashup_contest/E_Beza_and_the_clean_up.py

--- contest_20/E_Masha_and_a_Beautiful_Tree.py
+++ contest_20/E_Masha_and_a_Beautiful_Tree.py
@@ -1,8 +1,44 @@
-def solve(m,p):
-    pass
+
+
+
+def solve(nums):
+    count = 0
+    def sort(p):
+        if len(p)==1:
+            return p
+        
+        mid = len(p)//2
+      
+        left = sort(p[:mid])
+        right = sort(p[mid:])
+
+        res = left + right
+        nonlocal count
+        if right[-1]<left[-1]:
+            res = right + left
+            count+=1
+
+        return res
+    
+    ans = sort(nums)
+
+    if ans==sorted(nums):
+        return count
+    else:
+        return -1
+     
+    
+
+
 
 t = int(input())
 for _ in range(t):
     m = int(input())#power of two size of permutation
-    p = list(map(int,input().split()))
-    solve(m,p)
+    nums = list(map(int,input().split()))
+    print(solve(nums))
+    
+    # res= solve(p)
+    # if res==sorted(p):
+    #     print(count)
+    # else:
+    #     print(-1)
--- mashup_contest/E_Beza_and_the_clean_up.py
+++ mashup_contest/E_Beza_and_the_clean_up.py
@@ -1,14 +1,35 @@
-n, m, s, A, B = map(int, input().split())
-values_a = list(map(int, input().split()))
-values_b = list(map(int, input().split()))
+def solve(n,m,s,A,B,a_vals,b_vals):
+    #sort to ensure we pick vaues of larger values first
+    a_vals.sort(reverse=True)
+    b_vals.sort(reverse=True)
+    #use prefix sum to accumulate values of items
+    ps_a = [0]*(n+1)
+    ps_b = [0]*(m+1)
 
-max_total_cost = 0
+    for i in range(1,n+1):
+        ps_a[i]=a_vals[i-1]+ps_a[i-1]
 
-for a_count in range(n+1):
-    remaining_weight = s - (a_count * A)
-    if remaining_weight >= 0:
-        b_count = min(remaining_weight // B, m)
-        total_cost = sum(values_a[:a_count]) * A + sum(values_b[:b_count]) * B
-        max_total_cost = max(max_total_cost, total_cost)
+    for i in range(1,m+1):
+        ps_b[i]=b_vals[i-1]+ps_b[i-1]
 
-print(max_total_cost)
+    #anytime we pick a value of size A we look for a value of size B that can fit into the bag 
+    max_cost = 0
+   
+    for i in range(n+1):
+        rem_weight = s-i*A
+        if rem_weight<0:
+            break
+        #how many items of weight B can we take
+        num_B = min(rem_weight//B,m)
+        max_cost = max(max_cost,ps_a[i]+ps_b[num_B])
+
+    return max_cost
+
+
+
+n,m,s,A,B = map(int,input().split())
+a_values=list(map(int,input().split()))
+b_values = list(map(int,input().split()))
+
+
+print(solve(n,m,s,A,B,a_values,b_values))
