8a32deaa5027d41812c6a6a71531bf77c3ef8775
Tettey1 <tetteynathan89@gmail.com>
2023-12-01 12:23:21 +0000

    modified:   contest_22/C_Party.py 	new file:   contest_22/E_White_Black_Balanced_Subtrees.py

--- contest_22/C_Party.py
+++ contest_22/C_Party.py
@@ -1,12 +1,35 @@
 
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(2000)
+def dfs(num,graph):
+    if not num:
+        return 0
+    ans = 0
+    for x in graph[num]:
+        ans = max(ans,dfs(x,graph))
+
+    return ans+1
+
+    
 
 def solve():
     n = int(input())
-    hierarchy = []
+    arr = []
     for _ in range(n):
-        hierarchy.append(int(input()))
+        arr.append(int(input()))
+
+    tree = defaultdict(list)
+    for i,val in enumerate(arr):
+        if val!=-1:
+            tree[val].append(i+1) 
+
+    group = 0
+    for i,val in enumerate(arr):
+        if val==-1:
+            group =max(group,dfs(i+1,tree))
 
 
-
+    return group
 
 print(solve())
