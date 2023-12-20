e4484df716dbffcc7e66925dbbbcbf3514303334
Tettey1 <tetteynathan89@gmail.com>
2023-12-20 20:03:17 +0000

    modified:   contest_24/D_Anji_s_Binary_Tree.py 	modified:   contest_25/E_Anji_s_Binary_Tree.py

--- contest_24/D_Anji_s_Binary_Tree.py
+++ contest_24/D_Anji_s_Binary_Tree.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 import sys
 import threading
 
@@ -6,44 +6,80 @@ import threading
 def main():
     t = int(input())
     for _ in range(t):
-        n=int(input())
+        n = int(input())
         s = input()
-        s = list(s)
         graph = defaultdict(list)
         for i in range(n):
-            l,r = map(int, input().split())
+            l, r = map(int, input().split())
             graph[i+1].append(l)
             graph[i+1].append(r)
 
-        def dfs(node,count):     
-            if s[node-1]=='U' and node!=1:
-                if graph[node][0]!=0:
-                    s[node-1]='L'
-                    return dfs(graph[node][0],count+1)
+        min_op = float('inf')
+        queue = deque([(1, 0)])  # Start BFS from node 1 with count 0
 
-                if graph[node][1]!=0:
-                    s[node-1]='R'
-                    return dfs(graph[node][1],count+1)              
-            #if char is L and is no left change it
-            if s[node-1]=='L' and graph[node][0]==0:
-                s[node-1]='R'
-                return dfs(graph[node][1],count+1)
+        while queue:
+            node, count = queue.popleft()
 
-            if s[node-1]=='R' and graph[node][1]==1:
-                s[node-1]='L'
-                return dfs(graph[node][0],count+1)
+            if graph[node][0] == 0 and graph[node][1] == 0:
+                min_op = min(min_op, count)
+                
 
-            
+            if graph[node][0] != 0:
+                if s[node-1] == 'L':
+                    queue.append((graph[node][0], count))
+                else:
+                    queue.append((graph[node][0], count+1))
 
-            return count
+            if graph[node][1] != 0:
+                if s[node-1] == 'R':
+                    queue.append((graph[node][1], count))
+                else:
+                    queue.append((graph[node][1], count+1))
+
+        print(min_op)
+
+main()
 
 
-            
-        print(dfs(1,0))
+# def main():
+#     t = int(input())
+#     for _ in range(t):
+#         n=int(input())
+#         s = input()
+#         s = list(s)
+#         graph = defaultdict(list)
+#         for i in range(n):
+#             l,r = map(int, input().split())
+#             graph[i+1].append(l)
+#             graph[i+1].append(r)
 
-sys.setrecursionlimit(1 << 30)
-threading.stack_size(1 << 27)
-main_thread = threading.Thread(target=main)
-main_thread.start()
-main_thread.join()
+#         def dfs(node,count):   
+#             nonlocal min_op  
+#             if graph[node][0]==0 and graph[node][1]==0:
+#                 min_op = min(min_op,count)
+#                 return
+        
+#             #if the left node is L don't increment count 
+#             if graph[node][0] !=0:
+#                 if s[node-1]=='L':
+#                     dfs(graph[node][0],count)
+#                 else:
+#                     dfs(graph[node][0],count+1)
+#             if graph[node][1] !=0:
+#                 if s[node-1]=='R':
+#                     dfs(graph[node][1],count)
+#                 else:
+#                     dfs(graph[node][1],count+1)
 
+
+#         min_op = float('inf')
+#         dfs(1,0)
+#         print(min_op)
+
+        
+# main()
+# # sys.setrecursionlimit(1 << 30)
+# # threading.stack_size(1 << 27)
+# # main_thread = threading.Thread(target=main)
+# # main_thread.start()
+# # main_thread.join()
