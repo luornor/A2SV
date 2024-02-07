0ce59724c212e5f4a067e9f0ff37c912a0007328
Tettey1 <87794745+Tettey1@users.noreply.github.com>
2024-02-07 11:51:36 +0000

    Added solution for Magic Squares In Grid

--- leetcode-solutions/magic-squares-in-grid.py
+++ leetcode-solutions/magic-squares-in-grid.py
@@ -1,39 +1,47 @@
 class Solution:
-    
-    def is_magic_square(self,grid,start_row,start_col):
-        #row sum
-        row_sum = sum(grid[start_row][start_col:start_col+3])
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        def check_squares(r,c):
+            primary = grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2]
+            secondary = grid[r][c+2]+grid[r+1][c+1]+grid[r+2][c]
+            row_sum = sum(grid[r][c:c+3])
+            #distinct numbers
+            unique = set()
+            for i in range(r,r+3):
+                for j in range(c,c+3):
+                    if grid[i][j] in unique or grid[i][j] not in range(1,10):
+                        return False
+                    unique.add(grid[i][j])
+                    #rows
+                    if sum(grid[i][c:c+3]) !=row_sum:
+                        return False
+                    #cols
+                    col_sum = 0
+                    for x in range(r,r+3):
+                        col_sum+=grid[x][j]
+                    
+                    if col_sum!=row_sum:
+                        return False
 
-        hmap = collections.defaultdict(set)
-        # Check for distinct numbers and range validity
-        for i in range(start_row,start_row+3):
-            for j in range(start_col,start_col+3):
-                if grid[i][j] in hmap[i] or grid[i][j] not in range(1,10):
-                    return False
-                hmap[i].add(grid[i][j])
-                #if all row sum is equal
-                if sum(grid[i][start_col:start_col+3]) !=row_sum:
-                    return False
-                #if row is equal to all columns
-                if sum(grid[start_row][j] for start_row in range(start_row,start_row+3))!=row_sum:
-                    return False
-        #check each row each coloumn , each diagonal , each reverse diagonal
-        diagonal_sum =grid[start_row][start_col]+ grid[start_row+1][start_col+1]+grid[start_row+2][start_col+2]
-        reverse_diagonal_sum = grid[start_row][start_col+2]+ grid[start_row+1][start_col+1]+grid[start_row+2][start_col]
-        if diagonal_sum!= row_sum or reverse_diagonal_sum!= row_sum:
+            if primary!=row_sum or secondary!=row_sum:
                 return False
-        
-        return True
-    def numMagicSquaresInside(self,grid: List[List[int]]) -> int:
-        rows,cols = len(grid),len(grid[0])
-        count = 0       
-        for r in range(rows-2):
-            for c in range(cols-2):
-                if self.is_magic_square(grid,r,c):
-                    count+=1
-        
-        return count
 
+            return True
+                        
+        def is_valid(r,c):
+            return 0<=r <len(grid) and 0<= c <len(grid[0])
+        count = 0
+        for r in range(len(grid)-2):
+            for c in range(len(grid[0])-2):
+                if is_valid(r,c) and check_squares(r,c):
+                    count+=1
+
+
+        return count 
+
+
+
+            
+                
 
 
         
