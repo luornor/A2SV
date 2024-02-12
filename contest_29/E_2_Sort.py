a179e28f980b37aa0f6931ea8e26d36d3312fae1
Tettey1 <tetteynathan89@gmail.com>
2024-02-12 16:55:36 +0000

    modified:   contest_29/E_2_Sort.py

--- contest_29/E_2_Sort.py
+++ contest_29/E_2_Sort.py
@@ -2,11 +2,33 @@ def solve():
     n,k =map(int, input().split())
     a = list(map(int, input().split()))
     
-    
+    l = 0
+    count = 0
+    r = k
+    while r<n:
+        window = a[l:r+1]
+        for i in range(k+1):
+            window[i]=window[i]*2**i
+        
+        # print(window)
+        flag = True
+        for i in range(1,len(window)):
+            if window[i-1]>=window[i]:
+                flag = False
+
+        if flag:
+            count+=1
+        
+        l+=1
+        r+=1
 
 
-    
-    
+    return count
+
+
+
+
+
 
 t = int(input())
 for _ in range(t):
