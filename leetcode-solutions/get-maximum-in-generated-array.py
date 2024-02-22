0ca5801fcfe141d416f26fbb672e58762c545e6d
Tettey1 <87794745+Tettey1@users.noreply.github.com>
2024-02-22 22:54:12 +0000

    Added solution for Get Maximum in Generated Array

--- leetcode-solutions/get-maximum-in-generated-array.py
+++ leetcode-solutions/get-maximum-in-generated-array.py
@@ -1,21 +1,20 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        memo = {}
-        def dp(n):
-            if n<=1:
-                return n
+        if n <= 1:
+            return n
 
-            if n not in memo:
-                if n%2==0:
-                    memo[n] = dp(n//2)
-                else:
-                    memo[n] = dp((n-1)//2)+ dp((n+1)//2)
+        dp = [0] * (n + 1)
+        dp[1] = 1
 
-            return memo[n]
+        maxx = 1
 
-        maxx = 0
-        for i in range(n+1):
-            maxx = max(maxx, dp(i))
+        for i in range(2, n + 1):
+            if i % 2 == 0:
+                dp[i] = dp[i // 2]
+            else:
+                dp[i] = dp[(i - 1) // 2] + dp[(i + 1) // 2]
+
+            maxx = max(maxx, dp[i])
 
         return maxx
 
