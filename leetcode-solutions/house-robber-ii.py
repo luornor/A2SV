3b1954a67346a05a91a5dad390c82d3ad753c8d3
Tettey1 <87794745+Tettey1@users.noreply.github.com>
2024-02-22 22:57:31 +0000

    Added solution for House Robber II

--- leetcode-solutions/house-robber-ii.py
+++ leetcode-solutions/house-robber-ii.py
@@ -1,17 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)<=3:
+        n = len(nums)
+        if n <= 3:
             return max(nums)
 
-        def dp(l,r):
-            if l>r:
-                return 0
-            
-            if (l,r) not in memo:
-                memo[(l, r)] = max(dp(l+2,r)+nums[l],dp(l+1,r))
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-            return memo[(l, r)]
-        
-        n=len(nums)
-        memo = {}
-        return max(dp(0,n-2),dp(1,n-1))
+        for i in range(2, n - 1):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
+        max_amount1 = dp[n - 2]
+
+        dp = [0] * n
+        dp[1] = nums[1]
+
+        for i in range(2, n):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
+        max_amount2 = dp[n - 1]
+
+        return max(max_amount1, max_amount2)
