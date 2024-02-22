d516708d316adce6dbd92afdf701e59e290a8fd5
Tettey1 <87794745+Tettey1@users.noreply.github.com>
2024-02-22 17:13:10 +0000

    Added solution for N-th Tribonacci Number

--- leetcode-solutions/n-th-tribonacci-number.py
+++ leetcode-solutions/n-th-tribonacci-number.py
@@ -1,18 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        if n<2:
+            return n
 
-        memo = {}
-        def dp(n):
-            if n==0:
-                return 0
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
 
-            if n<=2:
-                return 1
+        print(dp)
+        return dp[-1]
 
-            if n not in memo:
-                memo[n] = dp(n-1) + dp(n-2) + dp(n-3)
-
-            return memo[n]
-
-        return dp(n)
         
