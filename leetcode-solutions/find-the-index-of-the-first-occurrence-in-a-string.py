628966c3aa1c37fad56b48d8630c7294794bf3e4
luornor <87794745+luornor@users.noreply.github.com>
2024-03-22 20:27:59 +0000

    Added solution for Find the Index of the First Occurrence in a String

--- leetcode-solutions/find-the-index-of-the-first-occurrence-in-a-string.py
+++ leetcode-solutions/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -9,28 +9,31 @@ class Solution:
         n = len(haystack)
         if m>n:
             return -1
+
         h = pow(a,m-1)%mod
-        needle_hash =haystack_hash = 0
+        needle_hash =0
+        haystack_hash = 0
 
         ref = ord('a')
         for i in range(m):
             encode_needle = ord(needle[i])-ref+1
             encode_haystack = ord(haystack[i])-ref+1
-            needle_hash = (a*needle_hash+encode_needle)%mod
-            haystack_hash = (a*haystack_hash+encode_haystack)%mod
+            needle_hash = (needle_hash+encode_needle*pow(a,m-i-1))%mod
+            haystack_hash = (haystack_hash+encode_haystack*pow(a,m-i-1))%mod
+        
+    
 
-        for  i in range(n-m+1):
-            if needle_hash==haystack_hash:
-                return i
-                
-            if i<n-m:
-                encode_haystack = ord(haystack[i])-ref+1
-                next_char = ord(haystack[i+m])-ref+1
-                haystack_hash=(a*(haystack_hash-encode_haystack*h) + next_char )%mod
-                
-                if haystack_hash<0:
-                    haystack_hash+=mod
+        if needle_hash==haystack_hash:
+            return 0
 
+        for i in range(m,n):
+            remove = ord(haystack[i-m])-ref+1
+            add = ord(haystack[i])-ref+1
+            haystack_hash=(a*(haystack_hash-remove*h) + add )%mod
+
+            if haystack_hash==needle_hash:
+                return i-m+1
+                
         return -1
 
         
