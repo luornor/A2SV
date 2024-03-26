b35fd8c69e5511942fb3e4a856608efc56010038
luornor <87794745+luornor@users.noreply.github.com>
2024-03-26 15:20:25 +0000

    Added solution for Longest Happy Prefix

--- leetcode-solutions/longest-happy-prefix.py
+++ leetcode-solutions/longest-happy-prefix.py
@@ -1,18 +1,22 @@
 class Solution:
     def longestPrefix(self, s: str) -> str:
+        a = 31 #base encode
+        mod = 10**9 + 7 #prime number
         n = len(s)
-        lps = [0]*n
+        def val(c):
+            return ord(c)-ord('a')+1
 
-        for i in range(1,n):
-            j = lps[i-1]
+        pre = 0
+        suff = 0
+        h = 1
+        ans=0
+        for i in range(n-1):
+            pre =  (pre*a + val(s[i])) % mod
+            suff = (suff + h* val(s[n-1-i]))%mod
 
-            while j>0 and s[i] != s[j]:
-                j = lps[j-1]
+            h = (h * a) % mod
+            if pre==suff:
+                ans = i+1
             
-            if s[i]==s[j]:
-                j+=1
-            
-            lps[i] = j
-        
-        # print(lps)
-        return s[:lps[n-1]]
+                    
+        return s[:ans]
